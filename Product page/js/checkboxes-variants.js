7c1eaeb240cff4e42e4585d0cfcd9756d66a88b1
MarkSuttirak <116248336+MarkSuttirak@users.noreply.github.com>
2022-12-06 14:11:11 +0700

    Add files via upload

---
+++ Product page/js/checkboxes-variants.js
@@ -0,0 +1,50 @@
+// Create check all functions
+
+let checkAll = document.getElementById("select-all-table");
+let checkboxesVariant = document.getElementsByClassName("check-stock");
+
+var fullPriceInput = document.getElementById("full-price-variant");
+var costInput = document.getElementById("cost-variant");
+var instockInput = document.getElementById("in-stock-variant");
+
+var fullPriceTable = document.getElementsByClassName("full-price-table");
+var costTable = document.getElementsByClassName("cost-table");
+var instockTable = document.getElementsByClassName("in-stock-table");
+
+function checkAllVariants(){
+    for (let i = 0; i < checkboxesVariant.length; i++){
+        checkboxesVariant[i].checked = true;
+    }
+}
+
+function uncheckAllVariants(){
+    for (let i = 0; i < checkboxesVariant.length; i++){
+        checkboxesVariant[i].checked = false;
+    }
+}
+
+checkAll.addEventListener("click", function(){
+    if (checkAll.checked){
+        checkAllVariants();
+    } else {
+        uncheckAllVariants();
+    }
+})
+
+function applyToSelection(){
+    
+}
+
+function applyToAll(){
+    for (let i = 0; i < fullPriceTable.length; i++){
+        fullPriceTable[i].value = fullPriceInput.value;
+    }
+
+    for (let i = 0; i < costTable.length; i++){
+        costTable[i].value = costInput.value;
+    }
+
+    for (let i = 0; i < instockTable.length; i++){
+        instockTable[i].value = instockInput.value;
+    }
+}
