7e5a6d0188f8e5dc7d08ad1ff499e1219ab6c3b0
MarkSuttirak <116248336+MarkSuttirak@users.noreply.github.com>
2022-12-06 09:48:29 +0700

    Update checkboxes-variants.js

--- Product page/js/checkboxes-variants.js
+++ Product page/js/checkboxes-variants.js
@@ -3,6 +3,14 @@
 let checkAll = document.getElementById("select-all-table");
 let checkboxesVariant = document.getElementsByClassName("check-stock");
 
+var fullPriceInput = document.getElementById("full-price-variant");
+var costInput = document.getElementById("cost-variant");
+var instockInput = document.getElementById("in-stock-variant");
+
+var fullPriceTable = document.getElementsByClassName("full-price-table");
+var costTable = document.getElementsByClassName("cost-table");
+var instockTable = document.getElementsByClassName("in-stock-table");
+
 function checkAllVariants(){
     for (let i = 0; i < checkboxesVariant.length; i++){
         checkboxesVariant[i].checked = true;
@@ -22,3 +30,22 @@ checkAll.addEventListener("click", function(){
         uncheckAllVariants();
     }
 })
+
+function applyToSelection(){
+    // I want full price, cost, and in-stock to be updated only when clicking "Apply to Selection" button and some variants are checked.
+}
+
+// For apply to all variants, there is no issue
+function applyToAll(){
+    for (let i = 0; i < fullPriceTable.length; i++){
+        fullPriceTable[i].value = fullPriceInput.value;
+    }
+
+    for (let i = 0; i < costTable.length; i++){
+        costTable[i].value = costInput.value;
+    }
+
+    for (let i = 0; i < instockTable.length; i++){
+        instockTable[i].value = instockInput.value;
+    }
+}
